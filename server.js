34b3b481cc790f8f7203d2cf7c53ddbfc752d2f8
vaibhav <singhvaibhav396@gmail.com>
2020-05-02 12:45:32 +0530

    h1

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('/test', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
